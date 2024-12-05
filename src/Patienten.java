2ee04d29b08c06b7004c4aa7f54a659fcd7d0a8c
valentina9vx <valentina.messnerx@gmail.com>
2024-12-05 11:21:56 +0100

    Patientenklasse

--- src/Patienten.java
+++ src/Patienten.java
@@ -1,2 +1,39 @@
 public class Patienten {
+
+    private String vorname;
+    private String nachname;
+    private int geburtsdatum;
+    private int svNummer;
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public int getGeburtsdatum() {
+        return geburtsdatum;
+    }
+
+    public void setGeburtsdatum(int geburtsdatum) {
+        this.geburtsdatum = geburtsdatum;
+    }
+
+    public int getSvNummer() {
+        return svNummer;
+    }
+
+    public void setSvNummer(int svNummer) {
+        this.svNummer = svNummer;
+    }
 }
